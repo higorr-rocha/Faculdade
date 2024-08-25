765ee7c3a07821fc9f642cde98ee98d491f74d64
Higor Rocha <72710977+Higorr-Rocha@users.noreply.github.com>
2024-08-25 03:32:44 -0300

    Organização

---
+++ 2º Semestre/Linguagem e Técnicas De Programação/Estudos/Lista em C/Lista - Ex 01.c
@@ -0,0 +1,24 @@
+/*
+    Disciplina  : [Linguagem e T�cnicas de Programação]
+    Nome        : Lista - Ex 01
+    Descrição   : Pede nome e idade de 3 pessoas. (Do While)
+    Autor       : Higor Rocha
+    Data atual  : 20/08/2024
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char nome[100];
+    int idade, cont=1;
+    do{
+        printf("Digite seu Nome: ");
+        fgets(nome, 100, stdin);
+        nome[strcspn(nome, "\n")] = '\0';
+        printf("Insira sua idade: ");
+        scanf("%d", &idade);
+        getchar();
+        cont ++;
+    }while(cont < 4);
+}
---
+++ 2º Semestre/Linguagem e Técnicas De Programação/Estudos/Lista em C/Lista - Ex 03.c
@@ -0,0 +1,23 @@
+/*
+    Disciplina  : [Linguagem e T�cnicas de Programação]
+    Nome        : Lista - Ex 03
+    Descrição   : Pede nome e idade de 3 pessoas. (For)
+    Autor       : Higor Rocha
+    Data atual  : 20/08/2024
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char nome[100];
+    int idade, i;
+    for(i = 1; i < 4; i++){
+        printf("Digite seu Nome: ");
+        fgets(nome, 100, stdin);
+        nome[strcspn(nome, "\n")] = '\0';
+        printf("Insira sua idade: ");
+        scanf("%d", &idade);
+        getchar();
+    }  
+}
