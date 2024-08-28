d2fb665a13d42fe32505810c649ecd7a8329b2a3
Higor Rocha <72710977+Higorr-Rocha@users.noreply.github.com>
2024-08-28 05:53:39 -0300

    Aula 08 - Prova Prática - Linguagens

---
+++ 2º Semestre/Linguagem e Técnicas De Programação/Aula 08 - Ex 03 - Prova Prática.c
@@ -0,0 +1,36 @@
+/*
+    Disciplina  : [Linguagem e Técnicas de Programação]
+    Nome        : Aula 08 - Exercício 03 - Prova Prática
+    Descrição   : Recebe 6 números e diz qual o maior sem usar laço de repetiçao e função.
+    Autor       : Higor Rocha
+    Data atual  : 24/08/2024
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main(){
+    int num, maior;
+    setlocale(LC_ALL, "Portuguese-Brazilian");
+    printf("Digite o 1º Número: ");
+    scanf("%d", &num);
+    maior = num;
+    printf("Digite o 2º Número: ");
+    scanf("%d", &num);
+    if(num > maior) {maior = num;}
+    printf("Digite o 3º Número: ");
+    scanf("%d", &num);
+    if(num > maior) {maior = num;}
+    printf("Digite o 4º Número: ");
+    scanf("%d", &num);
+    if(num > maior) {maior = num;}
+    printf("Digite o 5º Número: ");
+    scanf("%d", &num);
+    if(num > maior) {maior = num;}
+    printf("Digite o 6º Número: ");
+    scanf("%d", &num);
+    if(num > maior) {maior = num;}
+    printf("O maior número é %d.", maior);
+    return 0;
+}
