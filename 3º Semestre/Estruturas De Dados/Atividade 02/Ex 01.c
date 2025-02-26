92b630ea804a76401244d5f8c9e5c02f7aa1ebed
Higor Rocha <72710977+Higorr-Rocha@users.noreply.github.com>
2025-02-26 20:13:48 -0300

    Atividade 02 - Estrutura de Dados - 26/02/2025

---
+++ 3º Semestre/Estruturas De Dados/Atividade 02/Ex 01.c
@@ -0,0 +1,74 @@
+/*
+    Disciplina  : [Estruturas De Dados]
+    Nome        : Exercício 01
+    Autor       : Higor Rocha
+    Data atual  : 26/02/2025
+*/
+
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct veiculo
+{
+    char marca[20],modelo[30],cor[20],combustivel[15], placa[15];
+    short int ano_m,ano_f;
+};
+struct veiculo ve[5];
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    char placa_b[15];
+    short int i = 0;
+    
+    for (short int i = 0; i < 5; i++)
+    {
+        printf("Informe a marca do veículo %d:", i+1);
+        fgets(ve[i].marca, 20, stdin);
+        ve[i].marca[strcspn(ve[i].marca, "\n")] = 0;
+        printf("Informe o modelo do veículo %d:", i+1);
+        fgets(ve[i].modelo, 30, stdin);
+        ve[i].modelo[strcspn(ve[i].modelo, "\n")] = 0;
+        printf("Informe a cor do veículo %d:", i+1);
+        fgets(ve[i].cor, 20, stdin);
+        ve[i].cor[strcspn(ve[i].cor, "\n")] = 0;
+        printf("Informe o tipo de combustível do veículo %d:", i+1);
+        fgets(ve[i].combustivel, 15, stdin);
+        ve[i].combustivel[strcspn(ve[i].combustivel, "\n")] = 0;
+        printf("Informe a placa do veículo %d:", i+1);
+        fgets(ve[i].placa, 15, stdin);
+        printf("Informe o ano de fabricação do veículo %d:", i+1);
+        scanf("%hi", &ve[i].ano_f);
+        fflush(stdin);
+        printf("Informe o ano do modelo do veículo %d:", i+1);
+        scanf("%hi", &ve[i].ano_m);
+        fflush(stdin);
+    }
+    
+    printf("Informe a placa a ser buscada: ");
+    fgets(placa_b, 15, stdin);
+
+    do
+    {
+        if(strcmp(ve[i].placa,placa_b) == 0)
+        {
+            printf("Seu carro é o: %s\nMarca: %s\n", ve[i].modelo,ve[i].marca);
+            printf("Cor: %s\nPlaca: %s", ve[i].cor, ve[i].placa);
+            printf("Combustível utilizado: %s\n", ve[i].combustivel);
+            printf("Ano do Modelo: %hi\nAno de Fabricação: %hi\n", ve[i].ano_m, ve[i].ano_f);
+            i = 5;
+        }
+
+        i++;
+
+    } while(i < 5);
+    
+    if (i == 5)
+    {
+        printf("Veículo não localizado.");
+    }
+    
+    return 0;
+}
