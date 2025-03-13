521832bd7192bf2df79f219df23b689a30a1dd58
Higor Rocha <72710977+Higorr-Rocha@users.noreply.github.com>
2025-03-13 20:33:25 -0300

    Calculadora - Aula Front-End - 13/03/2025

---
+++ 3º Semestre/Programação Front End/Calculadora/scripts.js
@@ -0,0 +1,7 @@
+function soma() {
+    let campo_1 = document.getElementById('num_1');
+    let campo_2 = document.getElementById('num_2');
+    let resultado = parseFloat(campo_1.value) + parseFloat(campo_2.value);
+
+    
+}
